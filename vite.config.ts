ad180377f9caa580b60d6f3929a410b3132441aa
Алина <alina@MacBook-Pro-2.local>
2025-07-29 18:54:41 +0300

    **

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -13,6 +12,28 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  // Добавьте эти настройки для GitHub Pages
+  base: './', // Используем относительные пути
+  build: {
+    outDir: 'dist', // Папка для сборки
+    emptyOutDir: true, // Очищать папку при сборке
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', // Формат именования файлов
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js'
+      }
+    }
+  },
+  // Для разработки
+  server: {
+    port: 5173,
+    strictPort: true
+  },
+  preview: {
+    port: 4173,
+    strictPort: true
+  }
 })
